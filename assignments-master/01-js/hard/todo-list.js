36ba36e0736276fd4d1d91120089fc728ecbf1a4
Rahul Kumar Yadav <ry573870@gmail.com>
2025-01-03 09:19:09 +0530

    todo-list.js complete and all hard question given of assignment completed

--- assignments-master/01-js/hard/todo-list.js
+++ assignments-master/01-js/hard/todo-list.js
@@ -11,7 +11,52 @@
 */
 
 class Todo {
-
+  constructor(){
+    this.todos = [];
+    this.idx = 0;
+  }
+  add(str){
+    this.todos.push(str);
+    this.idx++;
+  }
+  remove(indexOfTodo){
+    if(indexOfTodo >=0 && indexOfTodo < this.todos.length){
+      this.todos.splice(indexOfTodo,1);
+      this.idx--;
+    }else{
+      return null;
+    }
+  }
+  update(index,updatedTodo){
+    if(index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+  }
+  getAll(){
+    return this.todos;
+  }
+  get(indexOfTodo){
+    if(indexOfTodo >= 0 && indexOfTodo < this.todos.length) return this.todos[indexOfTodo];
+    else return null;
+  }
+  clear(){
+    this.todos = [];
+    this.idx = 0;
+  }
 }
+//Manual testing
+let todoArray = new Todo();
+todoArray.add("Code");
+todoArray.add("Eat");
+todoArray.add("Sleep");
+todoArray.remove(2);
+todoArray.update(1,"Eat banana");
+todoArray.add("sleep");
+console.log(todoArray.getAll());
+console.log(todoArray.get(2));
+
+todoArray.clear();
+console.log(todoArray);
+
 
 module.exports = Todo;

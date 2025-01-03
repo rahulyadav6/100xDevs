9ef634d885557aa9aedd3f58f058272a5784aef0
Rahul Kumar Yadav <ry573870@gmail.com>
2025-01-03 09:20:25 +0530

    learn about regex

---
+++ Week_1/1.3/regex.js
@@ -0,0 +1,9 @@
+
+//website to learn about regular expressions(regex) : https://regexr.com/
+
+
+const regex = /very/g
+const text = "Rahul is a very very nice awesome nice very very nice boy";
+
+console.log(text.replace(regex, "excellent"));
+

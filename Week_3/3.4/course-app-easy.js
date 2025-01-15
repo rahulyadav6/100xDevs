d7a01c65f49fd275ad0fed489f89cccd7017d484
Rahul Kumar Yadav <ry573870@gmail.com>
2025-01-15 13:54:42 +0530

    admin course adding and updateing route completed

--- Week_3/3.4/course-app-easy.js
+++ Week_3/3.4/course-app-easy.js
@@ -9,6 +9,7 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+// Middleware to authenticate admin
 const adminAuthentication = (req,res,next)=>{
     const { username, password} = req.headers;
     const admin = ADMINS.find(a => a.username === username && a.password === password);
@@ -31,8 +32,42 @@ app.post("/admin/signup",(req,res)=>{
     }
 })
 
+// Admin long in route
 app.post("/admin/login", adminAuthentication, (req,res)=>{
     res.json({msg:"Logged in successfully"});
+})
+
+
+// Admin course adding route
+app.post("/admin/courses", adminAuthentication, (req,res)=>{
+    const course = req.body;
+    if(! course.title){
+        res.status(411).json({message: "Please provide title to the courese"});
+    }
+    if(! course.description){
+        res.status(411).json({message: "Please provide description to the courese"});
+    }
+    if(! course.price){
+        res.status(411).json({message: "Please provide price to the courese"});
+    }
+    if(! course.imageLink){
+        res.status(411).json({message: "Please provide imageLink to the courese"});
+    }
+    course.id = Date.now();
+    COURSES.push(course);
+    res.json({message: "Course created successfully", courseId: course.id});
+})
+
+// Admin course update route
+app.put("/admin/courses/:id", adminAuthentication, (req,res)=>{
+    const courseId = parseInt(req.params.id);
+    const course = COURSES.find(c=> c.id === courseId);
+    if(course){
+        Object.assign(course,req.body);
+        return res.json({message:"Course updated successfully"});
+    }else{
+        res.status(404).json({message:"Course not found"});
+    }
 })
 
 app.listen(port,()=>{

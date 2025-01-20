4f84f50df1e46a2357f4ff13379350c6a8cf0495
Rahul Kumar Yadav <ry573870@gmail.com>
2025-01-20 15:06:32 +0530

    revised with files seperated

--- assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,40 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    await Admin.create({
+        username,
+        password
+    })
+    res.json({message:"Admin created successfully"})
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCouse = await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+    res.json({msg:"Course created successfully", courseId: newCouse._id})
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+    const allCourses = await Course.find({});
+    res.json({courses: allCourses});
 });
 
 module.exports = router;

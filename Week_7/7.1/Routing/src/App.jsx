2165eaef6f7b7cf796ebeb953f388a937be32cb3
Rahul Kumar Yadav <ry573870@gmail.com>
2025-01-27 18:54:54 +0530

    7.1 completed

---
+++ Week_7/7.1/Routing/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Suspense } from 'react'
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
+const Dashboard = React.lazy(() =>import('./components/Dashboard'))
+const Landing = React.lazy(() =>import('./components/Landing'))
+// import Landing from './components/Landing'
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Appbar/>
+        <Routes>
+          <Route path='/dashboard' element={ <Suspense fallback={"Loading..."}> <Dashboard /></Suspense> }  />
+          <Route path='/' element={<Landing /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+function Appbar(){
+  const navigate = useNavigate();
+  return(
+    <div>
+      <div>
+        <button onClick={()=>{
+          // window.location.href = '/'
+          navigate("/");
+        }}>Landing</button>
+        <button onClick={()=>{
+          // window.location.href = '/dashboard'
+          navigate("/dashboard");
+        }}>Dashboard</button>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ Week_7/7.1/Routing/src/components/Landing.jsx
@@ -0,0 +1,8 @@
+function Landing(){
+    return(
+        <>
+            <h1>Landing page goes here</h1>
+        </>
+    );
+}
+export default Landing;

e02ecd9283ead197ab1112c66ed5994d7c78bc75
Rahul Kumar Yadav <ry573870@gmail.com>
2025-02-15 07:14:58 +0530

    routing in react complete

--- extras/roting/src/App.jsx
+++ extras/roting/src/App.jsx
@@ -1,19 +1,42 @@
 import './App.css'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
 function App() {
 
   return (
-    <>
+    <div>
     <BrowserRouter>
       <Routes>
-        <Route path='/neet/online-coaching-class-11' element={<Class11Program/>} />
-        <Route path='/neet/online-coaching-class-12' element={<Class12Program/>} />
-        <Route path='/' element={<Landing/>} />
+        <Route path='/' element={<Layout/>}>
+          <Route path='/neet/online-coaching-class-11' element={<Class11Program/>} />
+          <Route path='/neet/online-coaching-class-12' element={<Class12Program/>} />
+          <Route path='/' element={<Landing/>} />
+          <Route path='*' element={<ErrorPage/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
-    </>
+    </div>
   )
+}
+function Layout(){
+  return <div style={{height:"90vh"}}>
+    <Header/>
+    <div style={{height:"90vh"}}>
+      <Outlet/>
+    </div>
+    Footer | Contact us
+  </div>
+}
+function Header(){
+  return(
+    <div>
+      <Link to="/">Aleen</Link>
+        |
+        <Link to="/neet/online-coaching-class-11">Class 11</Link>
+        |
+        <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    </div>
+  );
 }
 function Landing(){
   return(
@@ -34,6 +57,13 @@ function Class12Program(){
     <>
       NEET programs for class 12.
     </>
+  );
+}
+function ErrorPage(){
+  return(
+    <div>
+      Sorry page not found
+    </div>
   );
 }
 

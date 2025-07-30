4c8712a8ff6ba86320e97a933b2fe293864a7e1c
Rahul Kumar Yadav <ry573870@gmail.com>
2025-07-30 08:42:40 +0530

    extras updated

---
+++ extras/three/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+function generateToken(){
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'x', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let token = "";
+    for(let i=0; i<32; i++){
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+app.post('/signup',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message:"You are signed in"
+    })
+})
+
+app.post('/signin',(req,res)=>{
+
+})
+
+app.listen(3000,()=>{
+    console.log(`Listening to port 3000`);
+})

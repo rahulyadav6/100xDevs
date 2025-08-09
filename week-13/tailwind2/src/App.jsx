f94dc5b2be74c1ff176b432b35e09305c49b6158
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-09 22:38:01 +0530

    use media query hook learn

--- week-13/tailwind2/src/App.jsx
+++ week-13/tailwind2/src/App.jsx
@@ -4,7 +4,14 @@ import Input from './components/Input'
 import Otp from './components/Otp'
 import Webinar from './Webinar'
 
+import { useMediaQuery } from "@uidotdev/usehooks";
+
 function App() {
+
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+  const isMediumDevice = useMediaQuery(
+    "only screen and (min-width : 769px) and (max-width : 992px)"
+  );
 
   return (
     <div className='h-screen bg-blue-700'>
@@ -12,7 +19,11 @@ function App() {
       {/* <Input type="text" placeholder={"Username"} ></Input>
       <Button disabled={true} >Sign Up</Button> */}
 
-      <Otp/>
+      {/* <Otp/> */}
+
+      <h1 className={isSmallDevice? "text-white" : "text-red-400"}>Hello</h1>
+      <h1 className={isMediumDevice? "text-white": "text-red-400"}>I am Rahul</h1>
+
     </div>
   )
 }
--- week-13/tailwind2/src/components/Button.jsx
+++ week-13/tailwind2/src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ disabled, children, onClick}){
     return(
         <>
             <span onClick={onClick} className={`text-white px-32 py-2 cursor-pointer rounded-2xl text-2xl ${disabled? "bg-blue-200" : "bg-green-400"}`}>
-                {children}
+                {children} 
             </span>
         </>
     );

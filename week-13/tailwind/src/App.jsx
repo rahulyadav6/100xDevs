18a80246cdb38e318369a59c1a78ba174a0ee5e4
Rahul Kumar Yadav <ry573870@gmail.com>
2025-08-09 22:22:55 +0530

    togglable sidebar complete

--- week-13/tailwind/src/App.jsx
+++ week-13/tailwind/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import MainContent from './components/MainContent'
 import Sidebar from './components/Sidebar'
+import Sidebar2 from './components/Sidebar2';
 
 function App() {
   const [sideBarOpen, setSideBarOpen] = useState(true);
 
   return (
     <div className='flex'>
-      <Sidebar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
-      <MainContent sideBarOpen={sideBarOpen} />
+      {/* <Sidebar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
+      <MainContent sideBarOpen={sideBarOpen} /> */}
+      <Sidebar2/>
     </div>
   )
 }

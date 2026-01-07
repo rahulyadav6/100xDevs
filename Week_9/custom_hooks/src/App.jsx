ae02691a4bcc6572f3a35a4f397fd278e889315c
Rahul Kumar Yadav <ry573870@gmail.com>
2026-01-08 01:23:57 +0530

    useTodos hooks complete

--- Week_9/custom_hooks/src/App.jsx
+++ Week_9/custom_hooks/src/App.jsx
@@ -1,24 +1,68 @@
-import  { useEffect  } from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const clock = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000)
+    axios.get("https://sum-server.100xdevs.com/todos")
+    .then(res =>{
+      setTodos(res.data.todos);
+      setLoading(false);
+    })
+    return()=>{
+      clearInterval(clock);
+    }
+  }, [n]);
+  
+  return { todos, loading };
+}
 
 const App = () => {
-  
+  // const [render, setRender] = useState(true);
+  // useEffect(()=>{
+  //   setInterval(()=>{
+  //     setRender(r=>!r);
+  //   },2000)
+  // },[]);
+
+  const { todos, loading } = useTodos(5);
+
   return (
     <>
-      <MyComponent/>
+      {/* {render?<MyComponent/>:null} */}
+      {loading ? "loading..." : todos.map((todo) => <Track todo={todo} />)}
     </>
-  )
+  );
+};
+
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
 }
 
-function MyComponent(){
-  useEffect(()=>{
+function MyComponent() {
+  useEffect(() => {
     console.log("Component mounted");
-    return()=>{
+    return () => {
       console.log("component unmounted");
-    }
-  },[])
-  return <div>
-    From inside my component
-  </div>
+    };
+  }, []);
+  return <div>From inside my component</div>;
 }
 
-export default App
+export default App;

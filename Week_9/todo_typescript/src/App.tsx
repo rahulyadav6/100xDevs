da9699ea35d1d32226014c0a37960f2596a691c4
Rahul Kumar Yadav <ry573870@gmail.com>
2026-01-09 23:15:57 +0530

    typescript in react | todo with types

---
+++ Week_9/todo_typescript/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Todo title='Code' description='Write some code' done={false} />
+    </>
+  )
+}
+
+interface TodoProp{
+  title:string,
+  description:string,
+  done:boolean
+}
+
+function Todo({title, description, done}: TodoProp){
+  return<div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+    <h3>{done?"Done":"Not Done"}</h3>
+  </div>
+
+}
+
+export default App

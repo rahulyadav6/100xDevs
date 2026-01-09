342344c3696033174a7491bc4fa593dd117ea911
Rahul Kumar Yadav <ry573870@gmail.com>
2026-01-09 21:58:16 +0530

    passing onjects | interfaces

--- Week_9/node-app/src/a.ts
+++ Week_9/node-app/src/a.ts
@@ -1,24 +1,45 @@
-function greet(firstName: string){
-    console.log(`Hello ${firstName}`)
+// function greet(firstName: string){
+//     console.log(`Hello ${firstName}`)
+// }
+
+// greet("Rahul") 
+
+// function sum(num1:number, num2:number): number {
+//     return num1 + num2;
+// }
+// console.log(sum(1,2));
+
+// function adult(age:number): boolean {
+//     return age > 18 ? true: false;
+// }
+// console.log(adult(60));
+
+// function higherOrderFunction(fn: ()=> void){
+//     setTimeout(fn,1000);
+
+// }
+
+// higherOrderFunction(function(){
+//     console.log("hi there");
+// })
+
+
+
+/* ---------------------------- interfaces -------------------------- */
+
+interface User{
+    firstName:string,
+    lastName: string,
+    age: number,
+    email?:string // email is optional
 }
 
-greet("Rahul") 
-
-function sum(num1:number, num2:number): number {
-    return num1 + num2;
-}
-console.log(sum(1,2));
-
-function adult(age:number): boolean {
-    return age > 18 ? true: false;
-}
-console.log(adult(60));
-
-function higherOrderFunction(fn: ()=> void){
-    setTimeout(fn,1000);
-
+function isLegal(user: User){
+    user.age > 18 ? true: false
 }
 
-higherOrderFunction(function(){
-    console.log("hi there");
+isLegal({
+    firstName:"Rahul",
+    lastName:"Yadav",
+    age:22
 })

ff82f5d538ce69f3c9ecfbeb5a82a6dd205c6aa6
Rahul Kumar Yadav <ry573870@gmail.com>
2026-01-23 23:56:36 +0530

    intersections and unions on types in typescript

--- Week-10/typescript_cohort3/src/index.ts
+++ Week-10/typescript_cohort3/src/index.ts
@@ -44,20 +44,82 @@
 
 //type in typescript
 
-interface Manager{
-    name:string,
-    age:number
+// interface Manager{
+//     name:string,
+//     age:number
+// }
+// interface Employee{
+//     name:string,
+//     department:string
+// }
+
+// type TeamLead = Manager & Employee
+
+// let t: TeamLead = {
+//     name:"Rahul",
+//     age:23,
+//     department:"Scinece"
+// }
+// console.log(t);
+
+
+// interface People{
+//     name: string,
+//     age:number,
+//     greet:()=>string,
+// }
+
+// const person1:People = {
+//     name:"Rahul",
+//     age:23,
+//     greet: ()=>{
+//         return "hello"
+//     }
+// } 
+
+// person1.greet();
+
+
+// interface User{
+//     name:string;
+//     age:number;
+//     isLegal():boolean;
+// }
+
+// class Manager implements User{
+//     name: string;
+//     age: number;
+//     constructor(name:string, age:number){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     isLegal(): boolean {
+//         return this.age > 18;
+//     }
+// }
+
+// unions and intersections in type
+type Employee = {
+    name:string;
+    startDate:string;
 }
-interface Employee{
-    name:string,
+type Manager = {
+    name:string;
     department:string
 }
+type TeamLead = Employee & Manager;
 
-type TeamLead = Manager & Employee
-
-let t: TeamLead = {
-    name:"Rahul",
-    age:23,
-    department:"Scinece"
+let e:Employee = {
+    name:"rahul",
+    startDate:"01-02,2004"
 }
-console.log(t);
+
+let m:Manager = {
+    name:"ak",
+    department:"development"
+}
+let t:TeamLead = {
+    name:"harkirat",
+    startDate:"02,01,2000",
+    department:"development"
+}

f3fc0c6a44e7a1d0bfa39444bc3a2582e6bcf22c
Rahul Kumar Yadav <ry573870@gmail.com>
2026-01-26 23:47:22 +0530

    prisma

---
+++ Week-10/prisma-postgres-ts/src/insert.ts
@@ -0,0 +1,17 @@
+import prisma from "./prisma.js";
+
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Rahul",
+      email: "rahul@gmail.com",
+    },
+  });
+
+  console.log("Inserted:", user);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
---
+++ Week-10/prisma-postgres-ts/src/prisma.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config"; // ⭐ REQUIRED
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});

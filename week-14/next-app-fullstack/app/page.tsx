f6d0d113816515ff533e3fd8e20fd34c56107bc5
Rahul Yadav <ry573870@gmail.com>
2026-02-10 09:21:27 +0000

    backend in nextjs

---
+++ week-14/next-app-fullstack/app/page.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+async function getUserDetails(){
+  await new Promise((r)=> setTimeout(r,5000));
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+  return response.data;
+}
+export default async function Home() {
+  const userData = await getUserDetails();
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>
+            Name: {userData?.name}
+          </div>
+          <div>
+            Email: {userData?.email}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

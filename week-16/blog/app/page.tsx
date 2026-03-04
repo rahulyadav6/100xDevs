15fd03c580321d97908eb1687a874a59444de861
Rahul Yadav <ry573870@gmail.com>
2026-03-04 17:58:15 +0000

    categories section complete

--- week-16/blog/app/page.tsx
+++ week-16/blog/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection"
+import CategoriesSection from "@/components/CategoriesSection"
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main>
         <HeroSection/>
         <FeaturedSection/>
+        <CategoriesSection/>
       </main>
     </div>
   );
--- week-16/blog/components/FeaturedSection.tsx
+++ week-16/blog/components/FeaturedSection.tsx
@@ -3,6 +3,7 @@ import * as motion from 'motion/react-client';
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { ArrowRight, Calendar } from "lucide-react";
+import Image from "next/image";
 export default function FeaturedSection(){
     const posts = featuredPosts;
     return(
@@ -33,7 +34,9 @@ export default function FeaturedSection(){
                             <Card className="group cursor-pointer overflow-hidden hover:shadow-xl transition-all duration-300 border-0 bg-card/50 backdrop:blur-xs py-0">
                                 <CardHeader className="p-0">
                                     <div className="relative">
-                                        <img src={post.image} alt={post.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105" />
+                                        <div className="w-full h-48 relative">
+                                        <Image fill src={post.image} alt={post.title} className=" object-cover transition-transform duration-300 group-hover:scale-105" />
+                                        </div>
                                         <div className="absolute top-4 left-4">
                                             <Badge variant="secondary" className="bg-background/80 backdrop-blur-xs">{post.category}</Badge>
                                         </div>
